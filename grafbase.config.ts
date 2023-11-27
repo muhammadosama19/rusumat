372ca519b4b3950298b55419a2612b60bd8d9bdd
muhammadosama19 <01128613705fm@gmail.com>
2023-11-27 21:10:54 +0200

    secound0000

--- grafbase.config.ts
+++ grafbase.config.ts
@@ -1,20 +1,5 @@
 import { g, config, auth } from '@grafbase/sdk';
 
-
-// @ts-ignore
-const User = g.model('User', {
-  name: g.string().length({ min: 2, max: 100 }),
-  email: g.string().unique(),
-  avatarUrl: g.url(),
-  description: g.string().length({ min: 2, max: 1000 }).optional(),
-  githubUrl: g.url().optional(),
-  linkedinUrl: g.url().optional(), 
-  projects: g.relation(() => Project).list().optional(),
-}).auth((rules) => {
-  rules.public().read()
-})
-
-// @ts-ignore
 const Project = g.model('Project', {
   title: g.string().length({ min: 3 }),
   description: g.string(), 
@@ -23,12 +8,23 @@ const Project = g.model('Project', {
   githubUrl: g.url(), 
   category: g.string().search(),
   createdBy: g.relation(() => User),
-}).auth((rules) => {
-  rules.public().read()
-  rules.private().create().delete().update()
 })
 
+const User = g.model('User', {
+  name: g.string().length({ min: 2, max: 100 }),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  description: g.string().length({ min: 2, max: 1000 }).optional(),
+  githubUrl: g.url().optional(),
+  linkedinUrl: g.url().optional(), 
+  projects: g.relation(() => Project).list().optional(),
+})
 
+const admin = g.model('Admin', {
+  name: g.string().optional(),
+  email: g.email().optional(),
+  gravatar: g.url().optional(),
+})
 export default config({
   schema: g
 })
