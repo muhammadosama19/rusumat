335513ee9abcb2a7d305f707a47970a255cfe587
muhammadosama19 <01128613705fm@gmail.com>
2023-11-27 15:54:10 +0200

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+export const metadata: Metadata = {
+  title: "Rusumat",
+  description: "Rusumat is Showcase desginer ui & ux & desginer",
+  keywords: "ui, ux, desgine, rusumat",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main>
+          {children}
+        </main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <section className="flexStart flex-col paddings mb-16">
+      <h1>catorgry</h1>
+      <h1>posts</h1>
+      <h1>LoadMore</h1>
+    </section>
+  )
+}
